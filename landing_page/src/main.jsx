e2dc57065c4add15639f7dbfd05d06f1de5c344e
Umang Mittal <117918427+umangmital24@users.noreply.github.com>
2026-09-09 10:53:34 +0530

    feat: route landing pages by pathname

--- landing_page/src/main.jsx
+++ landing_page/src/main.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import LandingPage from './LandingPage'
+import AboutPage from './AboutPage'
+import PrivacyPage from './PrivacyPage'
 import { Analytics } from '@vercel/analytics/react'
+
+function AppRouter() {
+  const path = window.location.pathname.replace(/\/+$/, '') || '/'
+
+  if (path === '/about') return <AboutPage />
+  if (path === '/privacy') return <PrivacyPage />
+  return <LandingPage />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage />
+    <AppRouter />
     <Analytics />
   </React.StrictMode>,
 )
